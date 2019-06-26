912fe9ec0e4b61e47b3a9e27bdda223fb82122e9
Tom DeVito <tdevito@icloud.com>
2019-06-26 07:53:12 -0400

    firebase and hosting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,21 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule, AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBeLCLcD6zpGDT-sfusG6fkYwDHduuIKuE",
+  authDomain: "giving-casino.firebaseapp.com",
+  databaseURL: "https://giving-casino.firebaseio.com",
+  projectId: "giving-casino",
+  storageBucket: "",
+  messagingSenderId: "511170909222",
+  appId: "1:511170909222:web:2e73628afb55c88b"
+};
 
 @NgModule({
   declarations: [
@@ -24,6 +36,7 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
+    AngularFireDatabase,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
